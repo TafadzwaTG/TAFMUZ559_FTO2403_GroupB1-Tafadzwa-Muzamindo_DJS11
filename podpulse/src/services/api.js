af35cda04d112e0da6771b06debc19c86caf9ba5
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2024-08-15 00:40:35 +0200

    add images, install Tailwind CSS, and create API service

---
+++ podpulse/src/services/api.js
@@ -0,0 +1,12 @@
+const BASE_URL = '`https://podcast-api.netlify.app'
+
+export const fetchShows = async () => {
+    const response = await fetch(BASE_URL);
+    if (!response.json);
+
+};
+ export const fetchShowByid = async (id) => {
+    const response = await fetch(`${BASE_URL}/id/${id}`);
+    if (!response.ok) throw new Error('Failed to fetch show');
+    return response.json():
+ }
